d0d28bcb6d609d8979570bfddc3309dac55d6468
attk <attk@DSV-Server.netgear.com>
2015-11-01 23:49:28 +0100

    anpassungen configs für erver
    SN

--- misc/bootstrap.sh
+++ misc/bootstrap.sh
@@ -4,15 +4,15 @@ touch /var/log/nginx/access.log
 touch /var/log/nginx/error.log
 touch /var/log/cron/owncloud.log
 
-if [ -z "$SSL_CERT" ]; then
+#if [ -z "$SSL_CERT" ]; then
     echo "\nCopying nginx.conf without SSL support..\n"
     cp /root/nginx.conf /etc/nginx/nginx.conf
-else
-    echo "\nCopying nginx.conf with SSL support..\n"
-    sed -i "s#-x-replace-cert-x-#$SSL_CERT#" /root/nginx_ssl.conf
-    sed -i "s#-x-replace-key-x-#$SSL_KEY#" /root/nginx_ssl.conf
-    cp /root/nginx_ssl.conf /etc/nginx/nginx.conf
-fi
+#else
+#    echo "\nCopying nginx.conf with SSL support..\n"
+#    sed -i "s#-x-replace-cert-x-#$SSL_CERT#" /root/nginx_ssl.conf
+#    sed -i "s#-x-replace-key-x-#$SSL_KEY#" /root/nginx_ssl.conf
+#    cp /root/nginx_ssl.conf /etc/nginx/nginx.conf
+#fi
 
 if [ "${OWNCLOUD_IN_ROOTPATH}" = "1" ]; then
     sed -i "s#-x-replace-oc-rootpath-#/var/www/owncloud/#" /etc/nginx/nginx.conf
